f530db10d4d5f7d5bb61c52a3ab280e6dcb10c5f
Albert (jiaming) Liu <70285611+albertNightingale@users.noreply.github.com>
2022-12-04 01:31:28 -0700

    implement the dynamic programming trick to back propagation and fix bugs, verified it against the writing problem

--- NN/NeuralNetwork.py
+++ NN/NeuralNetwork.py
@@ -1,4 +1,7 @@
 
+from typing import Union
+
+import math
 import numpy as np
 
 class Node: 
@@ -8,11 +11,16 @@ class Node:
     self.val = init_val
     self.loss_gradient = 0
 
+  def __str__(self):
+    return str(self.node_name)
+
+
 class NeuralNetwork:
-  def __init__(self, node_map: dict[str, Node], output_node: Node, point_to: np.ndarray, num_layers, num_nodes, num_input_nodes, method="sigmoid", epochs=100):
-    self.input_nodes = node_map
-    self.output_node = output_node
-    self.weights = point_to
+  def __init__(self, node_map: dict[str, Node], input_names: list[str], output_name: str, weights: np.ndarray, num_layers, num_nodes, num_input_nodes, method="sigmoid", epochs=100):
+    self.nodes = node_map
+    self.input_names = input_names
+    self.output_name = output_name
+    self.weights = weights
     
     self.num_layers = num_layers # number of layers
     self.num_nodes = num_nodes # total number of nodes
@@ -30,75 +38,110 @@ class NeuralNetwork:
     return self.gamma_0 / (1 + T)
 
   def _sigmoid(self, x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + math.exp(-x))
 
   def _sigmoid_derivative(self, val):
     sigmoid = self._sigmoid(val)
     return sigmoid * (1 - sigmoid)
 
   # forward pass
-  def forward_pass(self, x):
-    for i in range(self.num_input_nodes):
-      self.input_nodes[i].val = x[i] 
-    
-    q = self.input_nodes.tolist()
+  def forward_pass(self, x):    
+    # input nodes
+    for idx, input_name in enumerate(self.input_names):
+      self.nodes[input_name].val = x[idx]
 
-    # inverse bfs to fill out the value of all nodes
-    while len(q) > 0:
-      _node: Node = q.pop(0)
-      _links = self.weights[self.weights[:,1] == _node.node_name]
-      for _node_link in _links:
-        parent_name, w = _node_link[0], float(_node_link[2])
-        if self.output_node.node_name == parent_name:
-          self.output_node.val += _node.val * w
-        
-        # search in inner nodes
-        for _inner_node in self.inner_nodes:
-          if _inner_node.node_name == parent_name:
-            _inner_node.val += _node.val * w
-            q.append(_inner_node)
+    # inner nodes
+    for layer_number in range(1, self.num_layers, 1):
+      print("forward_pass: layer_number: ", layer_number)
+      for _name in self.nodes: 
+        if self.nodes[_name].layer_number != layer_number: # not the same layer number
+          continue
+        self.nodes[_name].val = self._compute_node(_name)
+    
+    # output node
+    output_node = self.nodes[self.output_name]
+    output_node.val = self._linear_combination(self.output_name)
 
   # compute the value of node_name by linear combination the values of the children
-  def _compute_node(self, node_name):
-    _matching_weights = self.weights[self.weights[:,0] == node_name] 
-    children_val: np.ndarray = np.array([self.input_nodes[child_name].val for child_name in _matching_weights[:,1]])
-    weights: np.ndarray = _matching_weights[:,2].astype(float)
+  def _compute_node(self, node_name: str) -> float:
+    lc = self._linear_combination(node_name)
+
+    if type(lc) == bool: # not computing the sigma because there is no children
+      return 1
+
+    sigma_lc = self._sigmoid(lc)
+    return sigma_lc
+
+  def _linear_combination(self, node_name: str) -> Union[float, bool]:
+    children_weights = self.weights[self.weights[:,0] == node_name] # get its childrens
+    print("childrens of {}: ".format(node_name))
+    print(children_weights)
+    children_val: np.ndarray = np.array([self.nodes[child_name].val for child_name in children_weights[:,1]]) # map children values
+    weights: np.ndarray = children_weights[:,2].astype(float) # map children weights
+    print("children_val: ", children_val)
+    print("weights: ", weights)
+
+    # if there is no children, return false
+    if children_val.shape[0] == 0:
+      return False
+
+    _dot = np.dot(children_val, weights)
+    print("resulting dot: {}".format(_dot))
+    print()
+    return _dot
   
-    return np.dot(children_val, weights)
-    
+  # compute the gradient of the weight under node with node_name
+  def _gradient_weight(self, node_name, _gradients, apply_activation: bool):
+    _node = self.nodes[node_name]
+    _filtered_indices = np.argwhere(self.weights[:,0] == node_name).flatten() # filter the weights between node and it's children
+
+    activation_derivative = 0
+    if apply_activation:
+      activation_derivative = self._sigmoid_derivative(self._linear_combination(node_name))
+      print("activation derivative of {} is {}".format(node_name, activation_derivative))
+
+    for i in _filtered_indices:
+      _child_name = self.weights[i][1]
+      _child_val = self.nodes[_child_name].val
+      
+      _grad = _node.loss_gradient * _child_val
+      if apply_activation:
+        _grad *= activation_derivative
+      _gradients[i] = _grad
+
   # gradient computation algorithm
   # assume the graph is already initialized using the forward pass with x
-  def _compute_gradient(self, y_node: Node, y_star):
+  def compute_gradient(self, y_star):
     # initialize an empty array of weights for gradient of each weight
     _gradients = np.zeros(self.weights.shape[0])
+    out_node = self.nodes[self.output_name]
 
     # compute the gradient L / gradient y
-    y_node.loss_gradient = y_node.val - y_star
-    y_node_idx = self.weights.argwhere(self.weights[:,0] == y_node.node_name)
-    # iterate all links and compute the gradient of each weight
-    for i in y_node_idx:
-      _child_name = self.weights[i,1]
-      _child_val = self.input_nodes[_child_name].val
-      _gradients[i] = y_node.loss_gradient * _child_val
-    
+    out_node.loss_gradient = out_node.val - y_star
+    self._gradient_weight(self.output_name, _gradients, False)
     
     # compute the gradient L / gradient x
-    for i in range(self.num_layers - 2, -1, -1):
-      for j in range(self.num_nodes[i]):
-        _node_name = f"layer_{i}_node_{j}"
-        _node = self.input_nodes[_node_name]
-        _node_idx = self.weights.argwhere(self.weights[:,0] == _node_name)
-        _node.loss_gradient = 0
-        for k in _node_idx:
-          _child_name = self.weights[k,1]
-          _child_val = self.input_nodes[_child_name].val
-          _child_loss_gradient = self.input_nodes[_child_name].loss_gradient
-          _gradients[k] = _child_loss_gradient * _child_val
-          _node.loss_gradient += _child_loss_gradient * self.weights[k,2]
+    for layer_number in range(self.num_layers - 1, 0, -1):
+      print("_compute_gradient: layer_number: ", layer_number)
+      for _name in self.nodes:
+        _node = self.nodes[_name] 
+        if _node.layer_number != layer_number: # not the same layer number
+          continue
         
-        _node.loss_gradient *= self._sigmoid_derivative(_node.val)    
+        print("processing node: ", _name)
+        # compute the gradient of the node
+        parents_weights = self.weights[self.weights[:,1] == _name]
+        
+        _parent_loss_grads = np.array([self.nodes[parent].loss_gradient for parent in parents_weights[:,0]]).flatten() # map parent's loss gradients
+        _parent_weights = parents_weights[:, 2].astype(float)
 
-    return 
+        _node.loss_gradient = np.dot(_parent_loss_grads, _parent_weights)
+        print("dot product parent_loss_grad {} with _parent_weights {} result in node gloss: {}\n".format(_parent_loss_grads, _parent_weights, _node.loss_gradient))
+
+        # compute the gradient of the weight
+        self._gradient_weight(_name, _gradients, True)
+
+    return _gradients
 
 
   # def fit(self, X, Y, initialize_weights=True):
--- tests/nn_tests1.py
+++ tests/nn_tests1.py
@@ -3,7 +3,9 @@ import numpy as np
 from NN.NeuralNetwork import NeuralNetwork
 from NN.NeuralNetwork import Node
 
-output_node = Node("y_1", 0, layer_number=3)
+output_node = "y_1"
+input_node = ["x_0", "x_1", "x_2"]
+
 node_map = {
   "x_0": Node("x_0", 1, layer_number=0),
   "x_1": Node("x_1", 1, layer_number=0), 
@@ -14,10 +16,10 @@ node_map = {
   "z_0_2": Node("z_0_2", 0, layer_number=2), 
   "z_1_2": Node("z_1_2", 0, layer_number=2), 
   "z_2_2": Node("z_2_2", 0, layer_number=2),
-  "y_1": output_node
+  "y_1": Node("y_1", 0, layer_number=3)
 }
 
-point_to = np.array([
+w = np.array([
   ["y_1", "z_0_2", -1], 
   ["y_1", "z_1_2", 2], 
   ["y_1", "z_2_2", -1.5], 
@@ -36,12 +38,31 @@ point_to = np.array([
 ])
 
 def main():
+  # format np printing
+  float_formatter = "{:.4f}".format
+  np.set_printoptions(formatter={'float_kind':float_formatter})
+  
   X = np.array([1, 1, 1])
   Y = np.array([1])
   # create neural network
-  nn = NeuralNetwork(node_map, output_node, point_to, num_layers=3, num_nodes=10, num_input_nodes=3)
+  nn = NeuralNetwork(node_map, input_node, output_node, w, num_layers=3, num_nodes=10, num_input_nodes=3)
   nn.forward_pass(X)
-  for weight in nn.weights:
-    print("weight pointing from {} to {} is {}".format(weight[1], weight[0], weight[2]))
-    g = nn._compute_gradient(weight, Y)
-    print("gradient is {}".format(g))
+  for _node_name in nn.nodes:
+    _node = nn.nodes[_node_name]
+    print("Node {} value is {:4f}".format(_node.node_name, _node.val))
+
+  print()
+
+  gradient_weights = nn.compute_gradient(Y[0])
+  
+  print()
+  print("SUMMARY: gradient_loss of each node:")
+  for _node_name in nn.nodes:
+    _node = nn.nodes[_node_name]
+    print("Node {} gloss is {:4f}".format(_node.node_name, _node.loss_gradient))
+  print()
+
+  for i in range(nn.weights.shape[0]):
+    weight = nn.weights[i]
+    grad: float = gradient_weights[i]
+    print("gradient weight pointing from {} to {} is {} ----- : {:4f}".format(weight[1], weight[0], weight[2], grad))
